8a23c1f8db9bf830cabc4989cd82db0d5d96ab7e
Prachiiiiii <97833293+prachibarnwal@users.noreply.github.com>
2022-12-29 21:48:20 +0530

    Add files via upload

---
+++ 307.) graph making.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+x = ["2019",'2020','2021','2022']
+y = [100,20000,50000,70000]
+
+plt.title("ANJALI LUTHRA")
+plt.xlabel("YEARS--")
+plt.ylabel("SUBSCRIBERS")
+plt.bar(x,y)
+plt.show()
